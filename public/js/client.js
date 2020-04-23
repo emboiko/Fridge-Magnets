fb8b990bb755bbb24ea74a833d36be1792189610
Emboiko <emboiko@gmail.com>
2020-04-23 03:42:11 -0400

    Initial Commit

---
+++ public/js/client.js
@@ -0,0 +1,127 @@
+const socket = io("http://localhost:3000");
+
+class Magnet{
+    constructor(x,y,radius, letter){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.letter = letter;
+    }
+
+    draw(){
+        fridge.c.beginPath();
+        fridge.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+        fridge.c.textAlign = "center";
+        fridge.c.strokeStyle = "#FFFFFF";
+        fridge.c.fillStyle = "#333333";
+        fridge.c.textBaseline = "middle";
+        fridge.c.font="40px Georgia";
+        fridge.c.fillText(this.letter, this.x, this.y);
+        fridge.c.stroke();
+    }
+}
+
+class Fridge{
+    constructor(){
+        this.canvas = document.getElementById("fridge");
+        this.c = this.canvas.getContext("2d");
+        this.canvas.width = 5000;
+        this.canvas.height = 4000;
+
+        this.magnets = [];
+    }
+}
+
+class Mouse{
+    constructor(){
+        self.x = undefined;
+        self.y = undefined;
+        self.dragging = false;
+        self.magnetIndex = 0;
+    }
+}
+
+const fridge = new Fridge();
+const mouse = new Mouse();
+
+///////////////////////////////////////////
+
+const header_height = document.getElementById("header").offsetHeight;
+
+fridge.canvas.addEventListener("mousedown", (e) => {
+    mouse.x = e.clientX + document.documentElement.scrollLeft;
+    mouse.y = e.clientY + document.documentElement.scrollTop - header_height;
+
+    fridge.magnets.forEach((magnet, index) => {
+    
+        if ((mouse.x >= magnet.x - magnet.radius) && (mouse.x <= magnet.x + magnet.radius)) {
+    
+            if ((mouse.y >= magnet.y - magnet.radius) && (mouse.y <= magnet.y + magnet.radius)) {
+                mouse.dragging = true;
+                mouse.magnetIndex = index;
+            }
+        }
+    });
+});
+
+fridge.canvas.addEventListener("mousemove", (e) => {
+    if (mouse.dragging){
+        const adjustedX = e.clientX + document.documentElement.scrollLeft;
+        const adjustedY = e.clientY + document.documentElement.scrollTop - header_height;
+        fridge.magnets[mouse.magnetIndex].x = adjustedX;
+        fridge.magnets[mouse.magnetIndex].y = adjustedY;
+
+        socket.emit("magnetMove", {
+            x:adjustedX,
+            y:adjustedY,
+            i:mouse.magnetIndex
+        });
+    }
+});
+
+fridge.canvas.addEventListener("mouseup", () => {
+    mouse.dragging = false;
+    mouse.draggingBackground = false;
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode == 87) document.documentElement.scrollTop -=35; //w
+    if (e.keyCode == 83) document.documentElement.scrollTop +=35; //s
+    if (e.keyCode == 81) document.documentElement.scrollLeft -=35; //q
+    if (e.keyCode == 65) document.documentElement.scrollLeft -=35; //a
+    if (e.keyCode == 69) document.documentElement.scrollLeft +=35; //a
+    if (e.keyCode == 68) document.documentElement.scrollLeft +=35; //d
+});
+
+//////////////////////////////////////////////
+
+socket.on("welcome", (magnets) => {
+    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollLeft = (fridge.canvas.offsetWidth / 2) -(window.innerWidth /2) ;
+
+    fridge.magnets = []; //Redundant, makes for a safe restart.
+    magnets.forEach((magnet) => {
+        fridge.magnets.push(new Magnet(magnet.x, magnet.y, magnet.radius, magnet.letter));
+    });
+});
+
+socket.on("update", (magnets) => {
+    fridge.magnets.forEach((magnet, i) => {
+        magnet.x = magnets[i].x;
+        magnet.y = magnets[i].y;
+    });
+});
+
+//////////////////////////////////////////////
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    fridge.c.clearRect(0, 0, fridge.canvas.width, fridge.canvas.height);
+
+    fridge.magnets.forEach((magnet) => {
+        magnet.draw();
+    });
+}
+
+animate();
---
+++ src/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const helmet = require("helmet");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(helmet());
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+const PORT = process.env.PORT;
+const expressServer = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const io = socketio(expressServer);
+
+
+class Magnet{
+    constructor(x,y,radius, letter){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.letter = letter;
+    }
+}
+
+class Fridge{
+    constructor(){
+        const radius = 30;
+        this.magnets = [];
+        
+        for (let i=0; i<500; i++) {
+            this.magnets.push(new Magnet(
+                Math.random() * (5000-radius*2) + radius,
+                Math.random() * (4000-radius*2) + radius,
+                radius,
+                this.generateCharacter()
+            ));
+        }
+    }
+
+    generateCharacter(){
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+}
+
+const fridge = new Fridge();
+
+io.on("connection", (socket) => {
+
+    socket.emit("welcome", fridge.magnets);
+
+    socket.on("magnetMove", (data) => {
+        fridge.magnets[data.i].x = data.x;
+        fridge.magnets[data.i].y = data.y;
+        socket.broadcast.emit("update", fridge.magnets);
+    });
+
+});
+
+/////////////////////////////
